c443bebcf6fbcd51c507d699d899bcb8b05fb5ea
Adam Gillessen <adamgillessen@gmail.com>
2017-09-05 19:15:56 +0100

    Makes the list of backups appear in revers-chronological order

--- webapp/backup_tools.py
+++ webapp/backup_tools.py
@@ -12,6 +12,6 @@ BACKUPS_DIR = p.BACKUPS_DIR
 
 def list_backups(username:str, timeframe:str) -> typing.List[str]:
     backups_base_dir = os.path.join(BACKUPS_DIR, username, timeframe)
-    all_backups = [b.strip(".tgz") for b in os.listdir(backups_base_dir) if 
-            re.match(r"^[0-9]{4}-[0-9]{2}-[0-9]{2}\.tgz", b)]
+    all_backups = sorted([b.strip(".tgz") for b in os.listdir(backups_base_dir) if 
+            re.match(r"^[0-9]{4}-[0-9]{2}-[0-9]{2}\.tgz", b)], reverse=True)
     return all_backups
