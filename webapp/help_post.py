680b2a1d6044ab781a6dc22625e56b550fb4c781
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2017-09-06 16:06:16 +0100

    Update help_post.py

--- webapp/help_post.py
+++ webapp/help_post.py
@@ -1,3 +1,8 @@
+'''
+This file takes care of sending off the data from the help section to multiple areas
+currently Discord and email of SysAdmins and the main Netsoc email
+'''
+
 from sendgrid import Email, sendgrid
 from sendgrid.helpers.mail import Content, Mail
 import string
@@ -5,6 +10,8 @@ import passwords as p
 import json
 import requests
 
+#Sends an email containing the help data to the various emails of SysAdmins etc
+#Will add more emails soon:tm:
 def send_help_email(username, email, subject_in, message):
     message_body = \
     """
@@ -25,6 +32,8 @@ Email: %s
 
     return str(response.status_code).startswith("20")
 
+#This sends the help data to the Netsoc Discord Bot, which will then post it in the relevant channel
+#in the Netsoc Committee Server
 def send_help_bot(username, email, subject, message):
     output = {"user":username, "email":email, "subject":subject, "message":message}
     url = "http://strum355.netsoc.co:4201/help"
