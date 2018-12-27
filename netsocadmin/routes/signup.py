0b11c11e6aa8828a41df768ec2913bc9ed506ec3
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2018-12-27 00:30:47 +0000

    Allow societies emails to signup

--- netsocadmin/routes/signup.py
+++ netsocadmin/routes/signup.py
@@ -122,7 +122,7 @@ class Confirmation(View):
         self.logger.debug("Received request")
         # make sure is ucc email
         email = flask.request.form['email']
-        if not re.match(r"[0-9]{9}@umail\.ucc\.ie", email):
+        if not re.match(r"[0-9]{9}@umail\.ucc\.ie", email) and not re.match(r"[a-z0-9]+@uccsocieties.com", email):
             self.logger.debug(f"Email {email} is not a valid UCC email")
             return flask.render_template("index.html", error_message="Must be a UCC Umail email address")
 
