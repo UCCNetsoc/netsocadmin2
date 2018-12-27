302266bc0046f2f6a74725d1d2549ecc8d4ce6e1
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2018-12-27 17:40:28 +0000

    Changed to .ie

--- netsocadmin/routes/signup.py
+++ netsocadmin/routes/signup.py
@@ -122,7 +122,7 @@ class Confirmation(View):
         self.logger.debug("Received request")
         # make sure is ucc email
         email = flask.request.form['email']
-        if not re.match(r"[0-9]{9}@umail\.ucc\.ie", email) and not re.match(r"[a-zA-Z0-9]+@uccsocieties.com", email):
+        if not re.match(r"[0-9]{9}@umail\.ucc\.ie", email) and not re.match(r"[a-zA-Z0-9]+@uccsocieties.ie", email):
             self.logger.debug(f"Email {email} is not a valid UCC email")
             return flask.render_template("index.html", error_message="Must be a UCC Umail or Society email address")
 
